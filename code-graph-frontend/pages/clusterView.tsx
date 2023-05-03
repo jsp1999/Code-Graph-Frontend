e8892a0cab82fac354196c8a564b81074bac6f10
Julia <131366468+jsp1999@users.noreply.github.com>
2023-05-03 13:41:06 +0200

    styled button

--- code-graph-frontend/pages/clusterView.tsx
+++ code-graph-frontend/pages/clusterView.tsx
@@ -4,7 +4,7 @@ export default function ClusterView() {
     return (   
     <div>
         <h1>Clusters are displayed here</h1>
-        <Link className="absolute right-5 bottom-5" href="/codeView">Change View</Link>
+        <Link className="absolute right-5 bottom-5 button" href="/codeView">Change View</Link>
     </div>
     )
 }
--- code-graph-frontend/pages/codeView.tsx
+++ code-graph-frontend/pages/codeView.tsx
@@ -4,7 +4,7 @@ export default function CodeView() {
     return (
         <div>
             <h1>Codes are displayed here</h1>
-            <Link className="absolute right-5 bottom-5" href="/clusterView">Change View</Link>
+            <Link className="absolute right-5 bottom-5 button" href="/clusterView">Change View</Link>
         </div>
     )
 }
