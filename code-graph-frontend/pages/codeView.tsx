cf77b96b3f1120ef5d233752cc21a2d83849ce2f
Julia <131366468+jsp1999@users.noreply.github.com>
2023-06-07 16:11:52 +0200

    minor adjustments

--- code-graph-frontend/pages/codeView.tsx
+++ code-graph-frontend/pages/codeView.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import data from "../src/data.json";
 
 export default function CodeView() {
-//    const map = new Map(Object.entries(JSON.parse(data)));
 
+    // its a little spaghetti but it works
     const jsonString = JSON.stringify(data, null, 2);
     const jsonData = JSON.parse(jsonString);
 
     // Map over the JSON object and access "daten" property
     const datenList = Object.keys(jsonData)
-        .slice(0, 20) // Slice the first ten elements
+        .slice(0, 25) // Slice the first ten elements
         .map(key => jsonData[key].daten);
 
     return (
@@ -17,7 +17,8 @@ export default function CodeView() {
             <header className="w-screen h-16 bg-blue-900 p-3 mb-5">
                 <h1 className="font-bold text-3xl">Code View</h1>
             </header>
-            <table className="table-auto w-[20%] max-h-screen float-left border-2 ml-3">
+            <div className="w-[20%] max-h-[600px] overflow-auto float-left ml-3">
+            <table className="table-auto border-2">
                 <thead className="border-2 bg-gray-100">
                 <tr>
                     <th>Code</th>
@@ -29,7 +30,9 @@ export default function CodeView() {
                 ))}
                 </tbody>
             </table>
-            <table className="table-auto w-[20%] max-h-screen float-right border-2 mr-3">
+            </div>
+            <div className="max-h-[400px] w-[20%] float-right mr-3 overflow-auto">
+            <table className="table-auto border-2">
                 <thead className="border-2 bg-gray-100">
                 <tr>
                     <th>Category</th>
@@ -51,6 +54,7 @@ export default function CodeView() {
 
                 </tbody>
             </table>
+            </div>
             <Link className="absolute right-5 bottom-5 button" href="/clusterView">Change View</Link>
         </div>
     )
