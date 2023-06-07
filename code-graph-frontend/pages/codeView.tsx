9f61d70724729d993bb4769be0e1ea86539aa630
Julia <131366468+jsp1999@users.noreply.github.com>
2023-06-07 15:52:57 +0200

    reading from json now

--- code-graph-frontend/pages/codeView.tsx
+++ code-graph-frontend/pages/codeView.tsx
@@ -3,6 +3,15 @@ import data from "../src/data.json";
 
 export default function CodeView() {
 //    const map = new Map(Object.entries(JSON.parse(data)));
+
+    const jsonString = JSON.stringify(data, null, 2);
+    const jsonData = JSON.parse(jsonString);
+
+    // Map over the JSON object and access "daten" property
+    const datenList = Object.keys(jsonData)
+        .slice(0, 20) // Slice the first ten elements
+        .map(key => jsonData[key].daten);
+
     return (
         <div>
             <header className="w-screen h-16 bg-blue-900 p-3 mb-5">
@@ -15,33 +24,9 @@ export default function CodeView() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                </tr>
-                <tr>
-                    <td>Witchy Woman</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                </tr>
-                <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                </tr>
-                <tr>
-                    <td>Witchy Woman</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                </tr>
-                <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                </tr>
-                <tr>
-                    <td>Witchy Woman</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                </tr>
+                {datenList.map((daten, index) => (
+                    <tr className="border" key={index}>{daten}</tr>
+                ))}
                 </tbody>
             </table>
             <table className="table-auto w-[20%] max-h-screen float-right border-2 mr-3">
