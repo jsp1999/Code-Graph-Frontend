12ce6f6ef844367dffe4cfdf0e3dd9cfe0759188
Julia <131366468+jsp1999@users.noreply.github.com>
2023-06-15 16:11:53 +0200

    updates

--- code-graph-frontend/components/Header.tsx
+++ code-graph-frontend/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return(
         <header className="w-screen h-16 bg-blue-900 p-3 mb-5">
-            <h1 className="font-bold text-3xl">"Code View</h1>
+            <h1 className="font-bold text-3xl">Code View</h1>
         </header>
     );
 }
