b958662ae81ecf03a0287c395a7bd68aa8d5689a
Julia Spahr <juliaspahr@web.de>
2023-09-20 21:20:48 +0200

    loading modal

---
+++ code-graph-frontend/components/LoadingModal.tsx
@@ -0,0 +1,24 @@
+import {
+    CircularProgress, Dialog, Modal,
+} from "@mui/material";
+import React from "react";
+
+interface LoadingModalProps {
+    open: boolean,
+}
+
+export default function LoadingModal(props: LoadingModalProps) {
+
+    return (
+        <>
+            <Modal
+                open={props.open}
+            >
+                <div className="w-fit bg-white p-5 rounded-lg shadow mx-auto mt-[15rem] grid-cols-2">
+                    <p>Loading</p>
+                    <CircularProgress />
+                </div>
+            </Modal>
+        </>
+    )
+}
