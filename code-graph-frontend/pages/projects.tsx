64efc6d99f09e0a825d08505305389eb4eb7d0ea
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-26 23:25:56 +0200

    feat(projects): display as tables

--- code-graph-frontend/pages/projects.tsx
+++ code-graph-frontend/pages/projects.tsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { getProjects, postProject, uploadAdvancedDataset, uploadDataset, uploadTestDataset } from "@/pages/api/api";
+import { getProjects } from "@/pages/api/api";
 import Header from "@/components/Header";
+import {Button,ButtonGroup} from "@mui/material";
+
+type Project = {
+  project_name: string;
+  project_id: number;
+  config_id: number;
+};
+
+
 
 export default function WelcomePage() {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState<Project[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const result = await getProjects();
-        let projectData = result.data.data;
+        let projectData: Project[] = result.data.data;
         setProjects(projectData);
       } catch (error) {
         console.error("Error fetching projects:", error);
@@ -21,29 +30,32 @@ export default function WelcomePage() {
 
   return (
     <div className="content-center">
-      <Header title="Code View"/>
-
+      <Header title="Code View" />
       <h1 className="w-fit mx-auto my-20 text-2xl">Welcome to CodeGraph!</h1>
       <div>
         <h2>List of Projects:</h2>
-        <ul>
-          {Array.isArray(projects) && projects.length > 0 ? (
-            projects.map((project) => (
-              <li key={project.project_id}>
-                <ul>
-                  {Object.entries(project).map(([key, value]) => (
-                    <li key={key}>
-                      <strong>{key}:</strong> {value}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))
-          ) : (
-            <li>No projects to display.</li>
-          )}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Project Name</th>
+              <th>Project ID</th>
+              <th>Config ID</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {projects.map((project) => (
+              <tr key={project.project_id}>
+                <td>{project.project_name}</td>
+                <td>{project.project_id}</td>
+                <td>{project.config_id}</td>
+                <td><Button>Dummy Button</Button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
+  
 }
