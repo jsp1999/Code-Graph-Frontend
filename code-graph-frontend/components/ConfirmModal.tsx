3ed5252ebe27bb48692a7a2ebdd48220a74b9af4
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-27 00:41:57 +0200

    feat(ConfirmModal): delete logic

--- code-graph-frontend/components/ConfirmModal.tsx
+++ code-graph-frontend/components/ConfirmModal.tsx
@@ -1,40 +1,48 @@
-import {Button, Modal} from "@mui/material";
+import { Button, Modal } from "@mui/material";
 import React from "react";
+import { getProjects, deleteProject, updateProject } from "@/pages/api/api";
 
-interface ConfirmModalProps {
-    open: boolean,
-    handleClose: () => void,
-}
+type ConfirmModalProps = {
+  open: boolean;
+  handleClose: () => void;
+  onDelete: (project_id: number) => Promise<any>;
+  projectId: number;
+};
 
 export default function ConfirmModal(props: ConfirmModalProps) {
-
-    const handleFinish = () => {
-        // Api call to delete project here
+  const handleFinish = async () => {
+    try {
+      await props.onDelete(props.projectId);
+      // Handle successful deletion
+    } catch (error) {
+      // Handle error
     }
+  };
 
-    function setClosed() {
-        props.handleClose();
-    }
+  function setClosed() {
+    props.handleClose();
+  }
 
-    return (
-        <>
-            <Modal
-                open={props.open}
-                onClose={setClosed}
+  return (
+    <>
+      <Modal open={props.open} onClose={setClosed}>
+        <div className="w-fit bg-white p-5 rounded-lg shadow mx-auto mt-[10vh] grid-cols-1 text-center">
+          <p>Do you want to delete this project?</p>
+          <div className="w-fit mx-auto mt-5">
+            <Button className="mx-2" variant="outlined" onClick={setClosed}>
+              No
+            </Button>
+            <Button
+              className="mx-2"
+              variant="contained"
+              component="label"
+              onClick={handleFinish}
             >
-                <div className="w-fit bg-white p-5 rounded-lg shadow mx-auto mt-[10vh] grid-cols-1 text-center">
-                <p>Do you want to delete this project?</p>
-                    <div className="w-fit mx-auto mt-5">
-                        <Button className="mx-2" variant="outlined" onClick={setClosed}>
-                            No
-                        </Button>
-                        <Button className="mx-2" variant="contained" component="label" onClick={handleFinish}>
-                            Yes
-                        </Button>
-                    </div>
-
-                </div>
-            </Modal>
-        </>
-    )
+              Yes
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
 }
