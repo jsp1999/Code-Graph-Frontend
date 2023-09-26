3d7cdb72b32ffe8f5f819d3246c00dd34be4c187
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-27 00:09:54 +0200

    feat(projects): implement tanstack and modals

--- code-graph-frontend/pages/projects.tsx
+++ code-graph-frontend/pages/projects.tsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from "react";
 import { getProjects } from "@/pages/api/api";
 import Header from "@/components/Header";
 import {Button,ButtonGroup} from "@mui/material";
+import {
+  getCoreRowModel,
+  ColumnDef,
+  flexRender,
+  useReactTable,
+} from '@tanstack/react-table'
+import EditModal from "@/components/EditModal";
+import Edit from '@mui/icons-material/Edit';
+import Delete from '@mui/icons-material/Delete';
+import ConfirmModal from "@/components/ConfirmModal";
+
+
 
 type Project = {
   project_name: string;
@@ -9,10 +21,47 @@ type Project = {
   config_id: number;
 };
 
+const columns: ColumnDef<Project>[] = [
+  {
+    header: 'Name',
+    footer: props => props.column.id,
+    columns: [
+      {
+        accessorFn: row => row.project_id,
+        id: 'project_id',
+        cell: info => info.getValue(),
+        header: () => <span>Project ID</span>,
+        footer: props => props.column.id,
+      },
+      {
+        accessorKey: 'project_name',
+        cell: info => info.getValue(),
+        footer: props => props.column.id,
+      },
 
+      {
+        accessorFn: row => row.config_id,
+        id: 'config_id',
+        cell: info => info.getValue(),
+        header: () => <span>Config ID</span>,
+        footer: props => props.column.id,
+      },
+    ],
+  },
+
+]
 
 export default function WelcomePage() {
+
   const [projects, setProjects] = useState<Project[]>([]);
+  const table = useReactTable({
+    columns,
+    data: projects,
+    getCoreRowModel: getCoreRowModel(),
+  })
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
+
 
   useEffect(() => {
     async function fetchData() {
@@ -29,33 +78,43 @@ export default function WelcomePage() {
   }, []);
 
   return (
-    <div className="content-center">
+    <header>
+    <ConfirmModal open={confirmModalOpen} handleClose={()=> setConfirmModalOpen(false)} />
+    <EditModal open={editModalOpen} handleClose={() => setEditModalOpen(false)} />
+
+    <div className="p-2 block max-w-full overflow-x-scroll overflow-y-hidden">
       <Header title="Code View" />
-      <h1 className="w-fit mx-auto my-20 text-2xl">Welcome to CodeGraph!</h1>
-      <div>
-        <h2>List of Projects:</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Project Name</th>
-              <th>Project ID</th>
-              <th>Config ID</th>
-              <th>Actions</th>
+      <div className="h-2" />
+      <table className="w-full ">
+        <thead>
+          <tr>
+            <th className="text-left">Project Name</th>
+            <th className="text-left">Project ID</th>
+            <th className="text-left">Config ID</th>
+            <th className="text-left">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <tr key={project.project_id}>
+              <td className="text-left">{project.project_name}</td>
+              <td className="text-left">{project.project_id}</td>
+              <td className="text-left">{project.config_id}</td>
+              <td className="text-left">
+              <div>
+                  <button onClick={() => setEditModalOpen(true)}>
+                      <Edit />
+                  </button>
+                  <button onClick={() => setConfirmModalOpen(true)}>
+                      <Delete />
+                  </button>
+              </div>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {projects.map((project) => (
-              <tr key={project.project_id}>
-                <td>{project.project_name}</td>
-                <td>{project.project_id}</td>
-                <td>{project.config_id}</td>
-                <td><Button>Dummy Button</Button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </div>
+  </header>
   );
-  
 }
