bd5b4a6b96dfc212c93b563d97c4d41bd6cdc49b
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-26 23:06:40 +0200

    feat(Header): ButtonGroup

--- code-graph-frontend/components/Header.tsx
+++ code-graph-frontend/components/Header.tsx
@@ -6,16 +6,24 @@ import EditModal from './EditModal';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Edit from '@mui/icons-material/Edit';
 import Delete from '@mui/icons-material/Delete';
+import {useRouter} from "next/router";
+import {Button,ButtonGroup} from "@mui/material";
+import {PiSuitcaseSimpleLight} from "react-icons/pi";
+import {FcScatterPlot} from "react-icons/fc";
+import {HiOutlineAnnotation} from "react-icons/hi";
+
 
 interface HeaderProps {
     title: string,
 }
 
 export default function Header(props: HeaderProps) {
+    const router = useRouter();
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const [editModalOpen, setEditModalOpen] = useState(false);
     const [confirmModalOpen, setConfirmModalOpen] = useState(false);
     const [projects, setProjects] = useState([""]);
+
 
 
     const toggleDrawer = () => {
@@ -69,6 +77,24 @@ export default function Header(props: HeaderProps) {
                             </div>
                         ))}
                         </Accordion>
+                    </div>
+                </div>
+                <div className="content-center">
+                    <div className="center">
+                        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" style={{ display: "flex", flexDirection: "column" }}>
+                        <Button variant="outlined" component="label" onClick={() => router.push(`/list`)} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <span>Projects</span>
+                            <PiSuitcaseSimpleLight />
+                        </Button>
+                        <Button variant="outlined" component="label" onClick={() => router.push(`/clusterView`)} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <span>Graph</span>
+                            <FcScatterPlot />
+                        </Button>
+                        <Button variant="outlined" component="label" onClick={() => router.push(`/codeView`)} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <span>Codes</span>
+                            <HiOutlineAnnotation />
+                        </Button>
+                        </ButtonGroup>
                     </div>
                 </div>
             </Drawer>
