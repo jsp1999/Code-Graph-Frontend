1f148a01755aa81e0a12c6aae3c9481338263693
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-30 21:38:06 +0200

    fix(stats): style for pie chart on a lot of clusters

--- code-graph-frontend/pages/stats.tsx
+++ code-graph-frontend/pages/stats.tsx
@@ -261,29 +261,29 @@ export default function StatsPage() {
             label: "Cluster Segment Count",
             data: data,
             backgroundColor: [
-              "rgb(255, 99, 132)",
-              "rgb(54, 162, 235)",
-              "rgb(75, 192, 192)",
-              "rgb(255, 205, 86)",
-              "rgb(201, 203, 207)",
-              "rgb(54, 162, 235)",
-              "rgb(255, 99, 132)",
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
             ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
           },
         ],
       };
 
-      const pieOptions = {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      };
-
       return (
         <div key={index} className="pie-chart">
-          <Pie options={pieOptions} data={pieData} />
+          <Pie data={pieData} />
         </div>
       );
     });
