de717e1efebdc36171c0c1b728f3cb467b56d597
Daniel Brühl <bruehl.dev@gmail.com>
2023-10-02 20:20:28 +0200

    refactor(codeView): apply linting

--- code-graph-frontend/pages/codeView.tsx
+++ code-graph-frontend/pages/codeView.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import { Button } from "@mui/material";
 import AddCodeModal from "@/components/AddCodeModal";
 import CodeTreeView from "@/components/CodeTreeView";
-import {getCodeTree, getconfig, refreshEntries} from "@/pages/api/api";
+import { getCodeTree, getconfig, refreshEntries } from "@/pages/api/api";
 import { useRouter } from "next/router";
 import LoadingModal from "@/components/LoadingModal";
 import * as d3 from "d3";
@@ -25,22 +25,23 @@ export default function CodeView() {
   const [openAddToCodeModal, setOpenAddToCodeModal] = useState(false);
   const [jsonData, setJsonData] = useState(data);
   const [loading, setLoading] = useState(false);
-  const [projectId, setProjectId] = useState(typeof window !== 'undefined' ? parseInt(localStorage.getItem("projectId") ?? "1"): 1);
+  const [projectId, setProjectId] = useState(
+    typeof window !== "undefined" ? parseInt(localStorage.getItem("projectId") ?? "1") : 1,
+  );
   const [selectedNodes, setSelectedNodes] = useState<number[]>(() => {
-    if (typeof window === 'undefined') {
-        // We're on the server, just return the default value
-        return [];
+    if (typeof window === "undefined") {
+      // We're on the server, just return the default value
+      return [];
     }
 
     // When component mounts, fetch the state from localStorage if it exists
-    const storedNodes = localStorage.getItem('selectedNodes');
+    const storedNodes = localStorage.getItem("selectedNodes");
     return storedNodes ? JSON.parse(storedNodes) : [];
-});
-
+  });
 
   useEffect(() => {
     // Any time selectedNodes changes, save it to localStorage
-      localStorage.setItem('selectedNodes', JSON.stringify(selectedNodes));
+    localStorage.setItem("selectedNodes", JSON.stringify(selectedNodes));
   }, [selectedNodes]);
   const handleOpen = () => setOpenAddToCodeModal(true);
 
@@ -49,7 +50,15 @@ export default function CodeView() {
       console.log("Initializing dot plotter...");
       const svg_ = d3.select(canvasRef.current);
       const container_ = d3.select("#container");
-      const newPlot = new CodeDotPlotter("container", projectId, "http://localhost:8000/", svg_, container_, selectedNodes, handleOpen);
+      const newPlot = new CodeDotPlotter(
+        "container",
+        projectId,
+        "http://localhost:8000/",
+        svg_,
+        container_,
+        selectedNodes,
+        handleOpen,
+      );
       fetchAndUpdateConfigs();
 
       setPlot(newPlot);
@@ -90,28 +99,27 @@ export default function CodeView() {
     setOpenAddToCodeModal(false);
     setLoading(true);
     getCodeTree(projectId)
-        .then((response) => {
-          setJsonData(response.data.codes);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+      .then((response) => {
+        setJsonData(response.data.codes);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   };
 
   const handleMergeModalClose = () => {
     setOpenMergeModal(false);
     setLoading(true);
     getCodeTree(projectId)
-        .then((response) => {
-          setJsonData(response.data.codes);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+      .then((response) => {
+        setJsonData(response.data.codes);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   };
-
 
   const handleUpdateSelectedNodes = (newSelectedNodes: number[]) => {
     setSelectedNodes(newSelectedNodes);
@@ -143,14 +151,23 @@ export default function CodeView() {
     }
   };
 
-
   return (
     <div>
       <Header title="Code View" />
       <AddCodeModal open={openAddModal} handleClose={handleAddModalClose} projectId={projectId} />
-      <AddToCodeModal open={openAddToCodeModal} handleClose={handleAddToCodeModalClose} projectId={projectId} codeId={rightClickedItemId} />
+      <AddToCodeModal
+        open={openAddToCodeModal}
+        handleClose={handleAddToCodeModalClose}
+        projectId={projectId}
+        codeId={rightClickedItemId}
+      />
       <LoadingModal open={loading} />
-      <MergeModal open={openMergeModal} handleClose={handleMergeModalClose} projectId={projectId} setLoading={() => setLoading(!loading)}/>
+      <MergeModal
+        open={openMergeModal}
+        handleClose={handleMergeModalClose}
+        projectId={projectId}
+        setLoading={() => setLoading(!loading)}
+      />
 
       <div className="float-left">
         <CodeTreeView
@@ -162,8 +179,7 @@ export default function CodeView() {
 
       <div>
         <svg id="canvas" ref={canvasRef} width="800" height="600">
-          <g id="container">
-          </g>
+          <g id="container"></g>
         </svg>
       </div>
 
@@ -174,11 +190,7 @@ export default function CodeView() {
         <Button variant="outlined" className="mr-10" onClick={() => setOpenAddModal(true)}>
           Add new Code
         </Button>
-        <Button
-          variant="contained"
-          className="bg-blue-900 rounded"
-          onClick={() => router.push(`/plotNeu_Comp`)}
-        >
+        <Button variant="contained" className="bg-blue-900 rounded" onClick={() => router.push(`/plotNeu_Comp`)}>
           Change View
         </Button>
       </div>
